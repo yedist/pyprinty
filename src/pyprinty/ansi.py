70a3ff902dddede0b7fad28688abfc40dc49f5ce
yedidya steinmetz <yedist10@gmail.com>
2026-03-03 01:35:51 +0200

    new Anci control codes

--- src/pyprinty/ansi.py
+++ src/pyprinty/ansi.py
@@ -1,4 +1,27 @@
 class Ansi:
-    custom_background_color = lambda r, g, b: f"\033[48;2;{r};{g};{b}m"
-    custom_text_color = lambda r, g, b: f"\033[38;2;{r};{g};{b}m"
-    bold = "\033[1m"
+    CUSTOM_BACKGROUND_COLOR = lambda r, g, b: f"\033[48;2;{r};{g};{b}m"
+    CUSTOM_TEXT_COLOR = lambda r, g, b: f"\033[38;2;{r};{g};{b}m"
+
+    BOLD = "\033[1m"
+    NO_BOLD = "\033[22m"
+    RESET_TEXT_EFFECTS = NO_BOLD
+
+    CLEAR_CURSOR_RIGHT = "\033[0K"
+    CLEAR_CURSOR_LEFT = "\033[1K"
+    CLEAR_CURSOR_DOWN = "\033[0J"
+    CLEAR_CURSOR_UP = "\033[1J"
+    CLEAR_TERMINAL = "\033c"
+    CLEAR_SCREEN = "\033[2J"
+    CLEAR_LINE = "\033[2K"
+
+    RESET_TEXT_COLOR = "\033[39m"
+    RESET_BACKGROUND = "\033[49m"
+    RESET_STYLE = "\033[0m"
+
+    CURSOR_MOVE = lambda row, col: f"\033[{row};{col}H"
+    CURSOR_UP = lambda n=1: f"\033[{n}A"
+    CURSOR_DOWN = lambda n=1: f"\033[{n}B"
+    CURSOR_RIGHT = lambda n=1: f"\033[{n}C"
+    CURSOR_LEFT = lambda n=1: f"\033[{n}D"
+    HIDE_CURSOR = "\033[?25l"
+    SHOW_CURSOR = "\033[?25h"
